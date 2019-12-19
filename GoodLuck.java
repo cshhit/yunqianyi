eed9c612fe3f923b9fc4d595479ccb6eba7c5976
cshhit <58712864+cshhit@users.noreply.github.com>
2019-12-19 11:48:21 +0800

    Update GoodLuck.java

--- GoodLuck.java
+++ GoodLuck.java
@@ -2,8 +2,9 @@
 public class GoodLuck {
 
 	public static void main(String[] args) {
+		System.out.println("Hello,world!");
 		System.out.println("Gook Luck");
-		System.out.println("Gook Luck");
+		System.out.println("Gook xzg");
 	}
 	
 }
